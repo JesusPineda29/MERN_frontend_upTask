1fb37655f7cbda39522475c547c8cc85b91ce2ec
JesusPineda29 <jesuspineda1429@gmail.com>
2025-07-09 15:24:58 -0600

    agregando colores a los estados

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -2,6 +2,7 @@ import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import { getProjectsById } from "@/api/ProjectAPI"
 import AddTaskModal from "@/components/tasks/AddTaskModal"
+import { TaskList } from "@/components/tasks/TaskList"
 
 
 
@@ -17,9 +18,9 @@ export const ProjectDetailsView = () => {
         retry: false
     })
 
-    if(isLoading) return 'Cargando...'
-    if(isError) return <Navigate to='/404' />
-    if(data) return (
+    if (isLoading) return 'Cargando...'
+    if (isError) return <Navigate to='/404' />
+    if (data) return (
         <>
             <h1 className="text-5xl font-black">{data.projectName}</h1>
             <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
@@ -28,11 +29,14 @@ export const ProjectDetailsView = () => {
                 <button
                     type="button"
                     className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-                    onClick={()=>navigate(location.pathname + '?newTask=true')}
+                    onClick={() => navigate(location.pathname + '?newTask=true')}
                 >Agregar Tarea</button>
             </nav>
 
 
+            <TaskList 
+                tasks={data.tasks}
+            />
 
             <AddTaskModal />
         </>
