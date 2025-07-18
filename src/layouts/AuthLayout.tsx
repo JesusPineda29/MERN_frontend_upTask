6c85bcba9d986474fecf7994409449a19db4a4bf
JesusPineda29 <jesuspineda1429@gmail.com>
2025-07-17 19:31:34 -0600

    añadiendo funcion para registrar la cuenta

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import { Logo } from '@/components/Logo';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 
 export const AuthLayout = () => {
@@ -13,6 +14,11 @@ export const AuthLayout = () => {
                     </div>
                 </div>
             </div>
+
+            <ToastContainer
+                pauseOnHover={false}
+                pauseOnFocusLoss={false}
+            />
         </>
     )
 }
