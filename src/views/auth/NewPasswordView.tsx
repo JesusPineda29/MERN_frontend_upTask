2853e7953185c14b61f8bb3f46a8f13c01936662
JesusPineda29 <jesuspineda1429@gmail.com>
2025-07-18 14:00:41 -0600

    definiendo la nueva ruta para restablecer el password

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,7 +1,22 @@
+import NewPasswordToken from "@/components/auth/NewPasswordToken"
+import { useState } from "react"
+import NewPasswordForm1 from "@/components/auth/NewPasswordForm";
 
 
 export const NewPasswordView = () => {
-  return (
-    <div>NewPasswordView</div>
-  )
+
+    const [isvalidToken, setIsvalidToken] = useState(false)
+
+
+    return (
+        <>
+            <h1 className="text-5xl font-black text-white">Restablecer Password</h1>
+            <p className="text-2xl font-light text-white mt-5">
+                Ingresa el código que recibiste {''}
+                <span className=" text-fuchsia-500 font-bold"> por emal</span>
+            </p>
+
+            {!isvalidToken ? <NewPasswordToken /> : <NewPasswordForm1 />}
+        </>
+    )
 }
