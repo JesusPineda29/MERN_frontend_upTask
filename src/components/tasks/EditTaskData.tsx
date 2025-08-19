9d82d2af1052b1e42b50cfa9dbca0f6a393ad3f2
JesusPineda29 <jesuspineda1429@gmail.com>
2025-08-19 12:43:38 -0600

    corriegiendo import

--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,7 +1,8 @@
 import { Navigate, useLocation, useParams } from 'react-router-dom';
 import { useQuery } from "@tanstack/react-query"
 import { getTaskById } from '@/api/TaskAPI';
-import EditTaskModal from '@/components/tasks/EditTaskModal';
+import EditTaskModal from './EditTaskModal';
+
 
 
 
